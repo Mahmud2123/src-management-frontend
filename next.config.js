e4962b8d59d38abce1aa66541c699c0fe89707e3
Mahmud2123 <hassanmahmud2123@gmail.com>
2026-07-01 22:30:54 -0700

    Fix: Add production API URL and fix rewrites

---
+++ next.config.js
@@ -0,0 +1,18 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    if (process.env.NODE_ENV === 'development') {
+      return {
+        beforeFiles: [
+          {
+            source: '/api/:path*',
+            destination: 'http://127.0.0.1:3001/api/:path*',
+          },
+        ],
+      };
+    }
+    return {};
+  },
+};
+
+export default nextConfig;
